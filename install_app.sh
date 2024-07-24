8350db946b87ce3737a9ba52294de64f8af17d5f
tin6150 <sn+newbox@grumpyxmas.com>
2024-07-24 14:42:01 -0700

    add elastic blast, it is an ncbi tool

--- install_app.sh
+++ install_app.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 pip3 install chewbbaca
+# need blast, trying:
+pip3 install elastic-blast==1.3.1 
+# might be looking for some blast db check tool, older version?  maybe in blast plus ?
